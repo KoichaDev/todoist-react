790b45f1fc9e09afa87e1bce58f7bfa71faced95
Krachi Crunch <khoii87@gmail.com>
2021-06-04 14:19:05 +0200

    Merge: 🔀 branch 'feature/component/navbar'
    Add: ✨ Header Component

---
+++ src/Components/UI/Header/NavBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './Header.module.scss';
+
+function NavBar() {
+  return <header className={classes.navbar}></header>;
+}
+
+export default NavBar;
---
+++ src/Components/UI/Main/Main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './Main.module.scss';
+
+function Main() {
+  return <main className={classes.main}>Main</main>;
+}
+
+export default Main;
