cdf01a3b5a946496009ce206ed27c92ad4f1006a
Krachi Crunch <khoii87@gmail.com>
2021-06-06 16:59:34 +0200

    Add: ✨ Multiple SVG icons

--- src/Components/Tasks/TasksList.js
+++ src/Components/Tasks/TasksList.js
@@ -5,10 +5,27 @@ import classes from './TasksList.module.scss';
 function TasksList() {
   const todoCtx = useContext(TodoContext);
 
-  console.log(todoCtx.items);
   const todoList = todoCtx.items.map((item) => {
     const { id, task } = item;
-    return <li key={id}>{task}</li>;
+    return (
+      <li key={id}>
+        <ul>
+          <li>
+            <label htmlFor='checkbox'></label>
+            <input type='checkbox' id='checkbox' />
+          </li>
+          <li>
+            <p>{task}</p>
+          </li>
+          <li>
+            <button>
+              <i class='far fa-edit'></i>
+              Edit
+            </button>
+          </li>
+        </ul>
+      </li>
+    );
   });
 
   return (
