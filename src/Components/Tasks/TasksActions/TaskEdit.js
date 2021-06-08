df476e7d0514416512b7a44c1bfd04e4e0a7aee6
Krachi Crunch <khoii87@gmail.com>
2021-06-08 18:39:00 +0200

    Add: ✨ pressing on the esc key will close the edit mode of the todo task

--- src/Components/Tasks/TasksActions/TaskEdit.js
+++ src/Components/Tasks/TasksActions/TaskEdit.js
@@ -1,9 +1,9 @@
 import { forwardRef } from 'react';
 import classes from './TaskEdit.module.scss';
 
-function TaskEdit({ onSubmit, task, value, onChange, onClick }, ref) {
+function TaskEdit({ onSubmit, task, value, onChange, onClick, onKeyPress }, ref) {
   return (
-    <form onSubmit={onSubmit} className={classes['edit-todo']}>
+    <form onSubmit={onSubmit} className={classes['edit-todo']} onKeyDown={onKeyPress}>
       <label htmlFor={`task-input-${task}`} />
       <input ref={ref} type='text' id={`task-input-${task}`} value={value} onChange={onChange} />
       <button
