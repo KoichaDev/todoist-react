9fdbe45b7e806d8cde76f90e0a276ac3d43b48c5
Krachi Crunch <khoii87@gmail.com>
2021-06-08 16:55:13 +0200

    Merge: 🔀 branch 'fix/component/EditLogic' to main
    Fix: 🐛 Input Element for changing the edit mode can't be submitted as empty value

--- src/Components/Tasks/TasksActions/TaskPriority.js
+++ src/Components/Tasks/TasksActions/TaskPriority.js
@@ -2,26 +2,34 @@ import React from 'react';
 import { CheckBoxIcon, UnCheckBoxIcon } from '../../UI/Icons/CheckBoxIcon';
 import classes from './TaskPriority.module.scss';
 
-function TasksPriority({ toggleEditMode, onClick, isCompleted }) {
+function TasksPriority({ toggleEditMode, onClick, isEditingMode, isCompleted }) {
+  let taskContent = '';
   const toggleCheckBoxIcon = isCompleted ? (
     <CheckBoxIcon className={classes['todo-list__action-checked-icon']} />
   ) : (
     <UnCheckBoxIcon className={classes['todo-list__action-unchecked-icon']} />
   );
-  return (
-    <div className={classes['todo-list__action-priority']}>
-      <button
-        type='button'
-        role='checkbox'
-        aria-checked='false'
-        aria-label='Mark task as Complete'
-        title='Mark as your Todo task is completed'
-        onClick={onClick}>
-        {toggleCheckBoxIcon}
-      </button>
-      {toggleEditMode}
-    </div>
-  );
+
+  if (!isEditingMode) {
+    taskContent = (
+      <>
+        <button
+          type='button'
+          role='checkbox'
+          aria-checked='false'
+          aria-label='Mark task as Complete'
+          title='Mark as your Todo task is completed'
+          onClick={onClick}>
+          {toggleCheckBoxIcon}
+        </button>
+        {toggleEditMode}
+      </>
+    );
+  } else {
+    taskContent = <>{toggleEditMode}</>;
+  }
+
+  return <div className={classes['todo-list__action-priority']}>{taskContent}</div>;
 }
 
 export default TasksPriority;
